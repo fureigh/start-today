36cf078227c75289f55179994c84c44d040db3af
Fureigh <info@fureigh.com>
2014-07-17 23:11:49 -0400

    Make routing point to actual index page.

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+def index():
+    return render_template('index.html')
 
 print os.environ['APP_SETTINGS']
 
